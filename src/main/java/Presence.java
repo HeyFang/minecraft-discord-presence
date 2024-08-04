fd5c3e4f6e11102fc75aff34c8afa51b7476655d
HeyFang <heyfang36@yahoo.com>
2024-08-04 19:14:23 +0530

    initial commit

---
+++ src/main/java/Presence.java
@@ -0,0 +1,17 @@
+package Presence;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Presence extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
